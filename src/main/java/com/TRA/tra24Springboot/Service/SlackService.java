e8dc83e30646aeab7253af410ccbebad259acd4b
Noura Said AL Saidi <97401813+NouraSaid@users.noreply.github.com>
2024-07-02 13:25:59 +0400

    fixed slack error

--- src/main/java/com/TRA/tra24Springboot/Service/SlackService.java
+++ src/main/java/com/TRA/tra24Springboot/Service/SlackService.java
@@ -6,29 +6,22 @@ import com.slack.api.methods.request.chat.ChatPostMessageRequest;
 import com.slack.api.methods.response.chat.ChatPostMessageResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 public class SlackService {
-
-
-
     @Value("${slack.token}")
     private String slackToken;
 
     public void sendMessage(String channel, String message) {
         Slack slack = Slack.getInstance();
-        String token = slackToken=""; // Your Slack API token
 
-        channel = "#noura";
-        message = "Hi";
         ChatPostMessageRequest request = ChatPostMessageRequest.builder()
                 .channel(channel)
                 .text(message)
                 .build();
 
         try {
-            ChatPostMessageResponse response = slack.methods(token).chatPostMessage(request);
+            ChatPostMessageResponse response = slack.methods(slackToken).chatPostMessage(request);
             if (response.isOk()) {
                 System.out.println("Message sent successfully to Slack!");
             } else {
@@ -39,14 +32,5 @@ public class SlackService {
         }
     }
 
-/*    public void sendMessage(String title, String message) {
-        RestTemplate restTemplate = new RestTemplate();
-        String payload = String.format("{\"text\": \"%s\\n%s\"}", title, message);
 
-        try {
-            restTemplate.postForEntity(slackToken, payload, String.class);
-        } catch (Exception e) {
-            System.out.println("Error sending message to Slack: " + e.getMessage());
-        }
-    }*/
 }
