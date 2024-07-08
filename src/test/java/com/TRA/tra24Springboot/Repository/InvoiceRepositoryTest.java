94e5159a60de4ba931d7b29a6c393c8ac47f04b6
Noura Said AL Saidi <97401813+NouraSaid@users.noreply.github.com>
2024-07-08 09:25:08 +0400

    write Low Stock Alert notification

---
+++ src/test/java/com/TRA/tra24Springboot/Repository/InvoiceRepositoryTest.java
@@ -0,0 +1,13 @@
+package com.TRA.tra24Springboot.Repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@DataJpaTest
+class InvoiceRepositoryTest {
+
+    @Test
+    void findInvoiceById() {
+    }
+}
