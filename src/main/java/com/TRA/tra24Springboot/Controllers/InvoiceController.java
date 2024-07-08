7f87f36cfcb3e245f2c23bf06d94c326e205e2be
Noura Said AL Saidi <97401813+NouraSaid@users.noreply.github.com>
2024-07-08 11:42:14 +0400

    add over due reminder method

--- src/main/java/com/TRA/tra24Springboot/Controllers/InvoiceController.java
+++ src/main/java/com/TRA/tra24Springboot/Controllers/InvoiceController.java
@@ -5,6 +5,7 @@ import com.TRA.tra24Springboot.Models.Product;
 import com.TRA.tra24Springboot.Service.InvoiceService;
 import com.TRA.tra24Springboot.Service.ProductService;
 import com.TRA.tra24Springboot.Service.SlackService;
+import com.TRA.tra24Springboot.Utils.DateHelperUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -38,6 +39,19 @@ public class InvoiceController {
                     .append(" is due on ")
                     .append(invoice.getDueDate().toString());
             SlackService.sendMessage("practice", message.toString());
+        }
+    }
+    @Scheduled(cron = "0 0 9 * * ?")
+    @PostMapping("overdue")
+    public void sendOverdueReminder(){
+        List<Invoice> overdueInvoices = invoiceService.getOverDueInvoices();
+        for (Invoice invoice : overdueInvoices) {
+            StringBuilder message = new StringBuilder();
+            message.append("Alert: Invoice #")
+                    .append(invoice.getId())
+                    .append(" is overdue since ")
+                    .append(invoice.getDueDate().toString());
+            slackService.sendMessage("practice", message.toString());
         }
     }
 }
